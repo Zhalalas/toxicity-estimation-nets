5e599d9ed29923a3ee688a5ed01414d0dba6e918
Zhalalas <148062582+Zhalalas@users.noreply.github.com>
2026-02-06 10:27:55 +0400

    Update first_network.py

--- models/first_network.py
+++ models/first_network.py
@@ -7,16 +7,14 @@ class FirstNetwork(nn.Module):
         self.layer2 = nn.Linear(16, 32)
         self.layer3 = nn.Linear(32, 16)
         self.output_layer = nn.Linear(16, 2)
-        self.tanh = nn.Tanh()
-        self.softmax = nn.Sigmoid()
+        self.ReLU = nn.ReLU()
     
     def forward(self, x):
         x = self.layer1.forward(x)
-        x = self.tanh.forward(x)
+        x = self.ReLU.forward(x)
         x = self.layer2.forward(x)
-        x = self.tanh.forward(x)
+        x = self.ReLU.forward(x)
         x = self.layer3.forward(x)
-        x = self.tanh.forward(x)
+        x = self.ReLU.forward(x)
         x = self.output_layer.forward(x)
-        x = self.softmax.forward(x)
         return x
